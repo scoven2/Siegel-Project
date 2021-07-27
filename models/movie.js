363d65dd2f8628b14f95f1a28564de2c2334e1ef
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-07-26 21:18:02 -0500

    created index, movie, purhcase_movie, and purchase models

---
+++ models/movie.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+    var Movie = sequelize.define("Movie", {
+        genre_ids: DataTypes.INTEGER,
+        id: DataTypes.INTEGER,
+        original_language: DataTypes.STRING,
+        original_title: DataTypes.STRING,
+        overview: DataTypes.STRING,
+        popularity: DataTypes.DECIMAL(10, 3),
+        poster_path: DataTypes.STRING,
+        release_date: DataTypes.STRING,
+        title: DataTypes.STRING,
+        vote_average: DataTypes.DECIMAL(10, 2),
+        vote_count: DataTypes.INTEGER,
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: new Date()
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            defaultValue: new Date()
+        }
+    });
+
+    Movie.associate = function(models) {
+        Movie.belongsToMany(models.Shoppingcart, { through: 'Shoppingcart_Movie' });
+        Movie.belongsToMany(models.Purchase, { through: 'Purchase_Movie' });
+    };
+
+    return Movie;
+};
---
+++ models/purchase_movie.js
@@ -0,0 +1,22 @@
+var db = require('../models');
+
+module.exports = function(sequelize, DataTypes) {
+    const Purchase_Movie = sequelize.define('Purchase_Movie', {
+        PurchaseId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: db.Purchase,
+                key: 'id'
+            }
+        },
+        MovieId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: db.Movie,
+                key: 'id'
+            }
+        }
+    });
+
+    return Purchase_Movie;
+}
