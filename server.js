b3dd776ba7c15b00848d42ad5cb0174c10576d07
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-27 12:59:06 -0500

    added require models for syncing as well as set up the express app to handle data parsing, and finally set up hadle bars

--- server.js
+++ server.js
@@ -5,9 +5,24 @@ var path = require("path")
 var session = require("express-sesion");
 var passport = require("./config/passport");
 
+// compress
 var compression = require('compression')
 
+// express app and listening port
 var app = express();
 var PORT = process.env.PORT || 8090;
 
+// compress all responses
 app.use(compression())
+
+// requiring our models for syncing
+var db = require("./models");
+
+// set up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// set up handlebars
+var exphbs = require("express-handlebars");
+
+//
