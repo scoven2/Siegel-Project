2f1014663d64b185a9dfe427e3ea924d82e68c98
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-07-27 22:44:46 -0500

    trying to push to main

--- config/passport.js
+++ config/passport.js
@@ -3,31 +3,30 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const db = require('../models');
 
-passport.use(new LocalStrategy(
-    {
+passport.use(new LocalStrategy({
         usernameField: 'email'
     },
     function(email, password, done) {
 
         db.User.findOne({
-            where: {
-                email: email
-            }
-        })
-        .then(function(dbUser) {
+                where: {
+                    email: email
+                }
+            })
+            .then(function(dbUser) {
 
-            if (!dbUser) {
-                return done(null, false, {
-                    message: 'incorrect email'
-                });
-            
-            } else if (!dbUser.validPassword(password)) {
-                return done(null, false, {
-                    message: 'incorrect password'
-                });
-            }
-            return done(null, dbUser);
-        });
+                if (!dbUser) {
+                    return done(null, false, {
+                        message: 'incorrect email'
+                    });
+
+                } else if (!dbUser.validPassword(password)) {
+                    return done(null, false, {
+                        message: 'incorrect password'
+                    });
+                }
+                return done(null, dbUser);
+            });
     }
 ));
 
