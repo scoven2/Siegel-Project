5296a3df1e0ff5ca2c5d27c2067bf5d96e63777a
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-29 20:22:32 -0500

    Trying to comment out the extra sessions

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.session());
 
 app.use(routes);
 
